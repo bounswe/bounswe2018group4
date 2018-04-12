872fb1fc6a16b52da069be4b5601d7d69ca6c59e
Yunus Ege Saygılı <32356164+ege-saygili-2015405147@users.noreply.github.com>
2018-04-12 15:31:34 +0300

    Added the get_trending_topics method

--- resources/TwitterAPI_Project/allAPIs/views.py
+++ resources/TwitterAPI_Project/allAPIs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 import tweepy
+import json
 from tweepy import OAuthHandler
 
 def retweet(request):
@@ -31,3 +32,43 @@ def home(request):
 def followerid_index(request):
 	return render(request,'followerid_index.html')
 	# Create your views here.
+
+def get_trending_topics(name):
+	"""
+		Author: Yunus Ege Saygılı
+
+		Returns top ten trending topics for the input location. If the location is invalid,
+		returns the top ten trending topics worldwide.
+	"""
+
+	# set up the consumer key and access token (obtained from https://apps.twitter.com/app/new)
+	consumer_key='Di6z6xXCwJrvduPmXSbNZdILG'
+	consumer_secret='VgfCkLt8QCmmVmBa8VJss6FQiE866L3Bs5DDSm8M9k5s3IDjwE'
+
+	access_token='121485173-PXoF8a0MkroQQisbN89BwntiLtZIe31nDZnXtJyH'
+	access_token_secret='CtHbI0YnoMCXMOxcEDYQZTMFqCRgXlJHYd3Umu1Pjpf2i'
+
+
+	#  get authorization from Twitter API using tweepy
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	auth.secure = True
+
+	api = tweepy.API(auth)
+
+	places = api.trends_available()
+	
+	woeid = 1 # woeid for worldwide is 1
+
+	for place in places: # search for the input parameter
+		if place['name'] == name: # if found, get the id of the given location
+			woeid = place['woeid']
+			break
+
+	trends = api.trends_place(woeid) # get the trending topics of the location
+	trendlist = []
+
+	for i in range(10) : # get the top ten tending topics
+		trendlist.append(trends[0]['trends'][i]['name'])
+	return trendlist
+
