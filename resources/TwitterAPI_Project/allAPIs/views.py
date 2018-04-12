ef52242b7479a57bb4ae067c00885121a25df797
Dilara Gökay <dilaragokay@gmail.com>
2018-04-13 01:10:03 +0300

    Add post function

--- resources/TwitterAPI_Project/allAPIs/views.py
+++ resources/TwitterAPI_Project/allAPIs/views.py
@@ -148,3 +148,18 @@ def view_following(screen_name):
         status=result_set_following[i]
         print("following"+str(i)+ " "+status._json['screen_name'] )
     return result_set_following
+
+def post(tweet):
+	"""Post a tweet in @MemoristBeaver account using Twitter API."""
+	
+	consumer_key='5GNyqW1qeECVJ6m3dzx6QGD3f'
+	consumer_secret='6Q7N0yZle5qvbDLqnIioRBXzkqODmSMdKSqxIHevrZKnHyWjkf'
+
+	access_token='981859542555348992-4VIhOkbS4MJru964CPgrGFteVQIzj1p'
+	access_token_secret='sUaIhvHW91KMgI1EqWijJzcbMMm6BLoSNd9HU76FCB4Go'
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+
+	api = tweepy.API(auth)
+
+	api.update_status(status=tweet)
