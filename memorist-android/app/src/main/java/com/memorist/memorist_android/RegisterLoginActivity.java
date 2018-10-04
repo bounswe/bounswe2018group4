0bcf4956a85560d3af5337a60bf3ee5b76b8453a
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-10-04 15:40:38 +0300

    Creation of the Android application.

---
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/RegisterLoginActivity.java
@@ -0,0 +1,13 @@
+package com.memorist.memorist_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_login);
+    }
+}
---
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
@@ -0,0 +1,11 @@
+package com.memorist.memorist_android.helper;
+
+/**
+ * All constants are stored in this class.
+ */
+public class Constants {
+
+    // The base API url which is used to make requests.
+    public static final String BASE_URL = "#    ";
+
+}
