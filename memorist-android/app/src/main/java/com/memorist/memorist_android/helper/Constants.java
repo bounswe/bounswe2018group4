0fbbafa461c839ea09cbb09e9469bf632c1027a0
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-10-05 23:02:39 +0300

    Apparently, android uses 10.0.0.2 as the localhost server. So api url is changed.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
@@ -6,7 +6,7 @@ package com.memorist.memorist_android.helper;
 public class Constants {
 
     // The API urls which are used to make requests.
-    public static final String API_BASE_URL = "http://127.0.0.1:8000";
+    public static final String API_BASE_URL = "http://10.0.2.2:8000";
     public static final String API_LOGIN = API_BASE_URL + "/auth/login";
 
 }
