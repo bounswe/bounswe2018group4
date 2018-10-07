aba7dc5f8e5efa50008e37f145e90febea96b305
Dilara Gokay <dilaragokay@gmail.com>
2018-10-08 00:54:27 +0300

    Add simple frontend app made via React

---
+++ resources/MemoristAPI/build/precache-manifest.c88d47ee7b65de13a2888c2fe49b1f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64b5b8846683e9b2dc41",
+    "url": "/static/js/main.64b5b884.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "64b5b8846683e9b2dc41",
+    "url": "/static/css/main.72a0f256.chunk.css"
+  },
+  {
+    "revision": "2f9df19322b3e1f9e2ed39cf30c8a7af",
+    "url": "/index.html"
+  }
+];
