173a0cd1027231e39e2d3ea423ea177ffbcf2335
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-10-08 16:41:48 +0300

    Register & Login activity has now control of register fragment

--- memorist-android/app/src/main/java/com/memorist/memorist_android/RegisterLoginActivity.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/RegisterLoginActivity.java
@@ -2,12 +2,14 @@ package com.memorist.memorist_android;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.memorist.memorist_android.fragment.LoginFragment;
+import com.memorist.memorist_android.fragment.RegisterFragment;
 import com.memorist.memorist_android.model.ApiResultUser;
 import com.memorist.memorist_android.ws.MemoristApi;
 
@@ -15,10 +17,12 @@ import io.realm.Realm;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RegisterLoginActivity extends BaseActivity
-    implements LoginFragment.OnFragmentInteractionListener {
+    implements LoginFragment.OnFragmentInteractionListener,
+        RegisterFragment.OnFragmentInteractionListener {
 
     private static final String TAG = "RegisterLoginActivity";
     private static final String TAG_LOGIN_FRAGMENT = "fragment_login";
+    private static final String TAG_REGISTER_FRAGMENT = "fragment_register";
 
     // The database manager used in application.
     private Realm realm;
@@ -59,7 +63,19 @@ public class RegisterLoginActivity extends BaseActivity
 
     @Override
     public void proceedToRegister() {
-        // TODO: Register Page is gonna be proceeded.
+        RegisterFragment fragment = RegisterFragment.newInstance();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim
+                .enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.registerLoginFragmentContent, fragment, TAG_REGISTER_FRAGMENT);
+        fragmentTransaction.addToBackStack(TAG_REGISTER_FRAGMENT);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void processRegister(String username, String password, String email, String firstName, String lastName) {
+        MemoristApi.registerNewUser(username, password, email, firstName, lastName, registerListener, registerErrorListener);
     }
 
     private Response.Listener<ApiResultUser> loginListener = new Response.Listener<ApiResultUser>() {
@@ -73,6 +89,21 @@ public class RegisterLoginActivity extends BaseActivity
         @Override
         public void onErrorResponse(VolleyError error) {
             Toast.makeText(getApplicationContext(), "Login is NOT successful", Toast.LENGTH_LONG).show();
+            Log.v("Error", error.toString());
+        }
+    };
+
+    private Response.Listener<ApiResultUser> registerListener = new Response.Listener<ApiResultUser>() {
+        @Override
+        public void onResponse(ApiResultUser response) {
+            Toast.makeText(getApplicationContext(), "Register is successful", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private Response.ErrorListener registerErrorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(getApplicationContext(), "Register is NOT successful", Toast.LENGTH_LONG).show();
             Log.v("Error", error.toString());
         }
     };
