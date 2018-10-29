8742059b52c23a8d192e4b8d44fa289a8233d95e
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-10-29 12:48:37 +0300

    Unnecessary import is removed.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/fragment/FeedMemoryFragment.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/fragment/FeedMemoryFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
