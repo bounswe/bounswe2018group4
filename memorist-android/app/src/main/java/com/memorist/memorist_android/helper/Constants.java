9e23db76070782c2f07b4cca9250254665b7f6c6
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-10-29 12:26:33 +0300

    Api url addresses are updated.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     // The API urls which are used to make requests.
     private static final String API_BASE_URL = "http://10.0.2.2:8000";
-    public static final String API_LOGIN = API_BASE_URL + "/auth/login";
-    public static final String API_REGISTER = API_BASE_URL + "/auth/register";
+    public static final String API_LOGIN = API_BASE_URL + "/auth/login/";
+    public static final String API_REGISTER = API_BASE_URL + "/auth/register/";
 
 }
