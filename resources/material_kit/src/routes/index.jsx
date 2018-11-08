be7adc84bfcd47e7b402edcceafac801714b406d
busra hilal <b.hilal.c1@gmail.com>
2018-11-08 23:24:05 +0300

    Integrated material-ui kit with backend
    Added login, sign-up pages and functionalities. Added profile page also without functionality

---
+++ resources/material_kit/src/routes/index.jsx
@@ -0,0 +1,15 @@
+import Components from "views/Components/Components.jsx";
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "views/LoginPage/LoginPage.jsx";
+import SignUpPage from "views/SignUpPage/SignUpPage.jsx";
+
+var indexRoutes = [
+  { path: "/landing-page", name: "LandingPage", component: LandingPage },
+  { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
+  { path: "/login-page", name: "LoginPage", component: LoginPage },
+  { path: "/signup-page", name: "SignUpPage", component: SignUpPage },
+  { path: "/", name: "Components", component: Components }
+];
+
+export default indexRoutes;
