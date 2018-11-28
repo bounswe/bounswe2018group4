dd74f83278f450da8e954b4a55000ce6e6451b5a
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-11-28 21:10:57 +0300

    New create memory url is changed

--- memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/helper/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     private static final String API_BASE_URL = "http://18.234.162.48:8000";
     public static final String API_LOGIN = API_BASE_URL + "/auth/login/";
     public static final String API_REGISTER = API_BASE_URL + "/auth/register/";
-    public static final String API_CREATE_MEMORY = API_BASE_URL + "/post/create/1/";
+    public static final String API_CREATE_MEMORY = API_BASE_URL + "/post/create1/";
     public static final String API_CREATE_PHOTO = API_BASE_URL + "/post/media/1/";
     public static final String API_CREATE_VIDEO = API_BASE_URL + "/post/media/2/";
     public static final String API_CREATE_AUDIO = API_BASE_URL + "/post/media/3/";
