6de2f1eecbf6686085c6f122b01daa4c26687da9
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-11-29 14:04:17 +0300

    Token was in format, "JWT " + userToken. Now fixed.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/RegisterLoginActivity.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/RegisterLoginActivity.java
@@ -104,7 +104,7 @@ public class RegisterLoginActivity extends BaseActivity
         @Override
         public void onResponse(ApiResultUser response) {
             String userWelcome = "Welcome, " + response.getUser().getFirst_name() + " " + response.getUser().getLast_name();
-            String userToken = response.getToken();
+            String userToken = "JWT " + response.getToken();
 
             SharedPrefHelper.setUserToken(getApplicationContext(), userToken);
             Toast.makeText(getApplicationContext(), userWelcome, Toast.LENGTH_LONG).show();
