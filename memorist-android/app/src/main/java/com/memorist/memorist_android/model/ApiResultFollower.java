361ae021902cb2a2bb93c95d6ab58e6dec9273ce
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2018-12-24 17:11:27 +0300

    Follower / following now have usernames.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/model/ApiResultFollower.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/model/ApiResultFollower.java
@@ -7,8 +7,12 @@ public class ApiResultFollower {
     @Expose
     private int follower;
 
-    public ApiResultFollower(int follower) {
+    @Expose
+    private String username;
+
+    public ApiResultFollower(int follower, String username) {
         this.follower = follower;
+        this.username = username;
     }
 
     public int getFollower() {
@@ -17,5 +21,13 @@ public class ApiResultFollower {
 
     public void setFollower(int follower) {
         this.follower = follower;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
