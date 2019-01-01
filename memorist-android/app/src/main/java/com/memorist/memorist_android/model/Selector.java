d5cf311d5d9703f09d0ef28e07f1b13b676a5ade
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-01-01 23:48:09 +0300

    Selector model is implemented.

---
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/model/Selector.java
@@ -0,0 +1,39 @@
+package com.memorist.memorist_android.model;
+
+import com.google.gson.annotations.Expose;
+
+class Selector {
+
+    @Expose
+    private String s_type;
+
+    @Expose
+    private String start;
+
+    @Expose
+    private String end;
+
+    public String getEnd() {
+        return end;
+    }
+
+    public String getS_type() {
+        return s_type;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public void setS_type(String s_type) {
+        this.s_type = s_type;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+}
