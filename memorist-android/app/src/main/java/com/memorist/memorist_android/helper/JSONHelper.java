4390330709a25404322a8e84340d6c87f94c62d4
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-01-01 17:31:44 +0300

    A json helper function is implemented to create location json objects.

--- memorist-android/app/src/main/java/com/memorist/memorist_android/helper/JSONHelper.java
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/helper/JSONHelper.java
@@ -1,6 +1,8 @@
 package com.memorist.memorist_android.helper;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -21,6 +23,36 @@ public class JSONHelper {
 
         for(int element : list) {
             arr.put(element);
+        }
+
+        return arr;
+    }
+
+    public static JSONArray listToJSONArrayForLocation(String location1, String location2) {
+        JSONArray arr = new JSONArray();
+        JSONObject obj1 = new JSONObject();
+        JSONObject obj2 = new JSONObject();
+
+        if(!location1.equals("")) {
+            try {
+                obj1.put("location_name", location1);
+                obj1.put("location_coordinate_latitude", 37.23);
+                obj1.put("location_coordinate_longitude", 29.12);
+                arr.put(obj1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(!location2.equals("")) {
+            try {
+                obj2.put("location_name", location2);
+                obj2.put("location_coordinate_latitude", 37.32);
+                obj2.put("location_coordinate_longitude", 29.14);
+                arr.put(obj2);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
         return arr;
