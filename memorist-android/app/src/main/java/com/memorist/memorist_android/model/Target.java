e1c2d002274a505a62e706e2283deb8a4f275745
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-01-01 23:47:55 +0300

    Target model implemented.

---
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/model/Target.java
@@ -0,0 +1,39 @@
+package com.memorist.memorist_android.model;
+
+import com.google.gson.annotations.Expose;
+
+public class Target {
+
+    @Expose
+    private String t_type;
+
+    @Expose
+    private String source;
+
+    @Expose
+    private Selector selector;
+
+    public Selector getSelector() {
+        return selector;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getT_type() {
+        return t_type;
+    }
+
+    public void setSelector(Selector selector) {
+        this.selector = selector;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setT_type(String t_type) {
+        this.t_type = t_type;
+    }
+}
