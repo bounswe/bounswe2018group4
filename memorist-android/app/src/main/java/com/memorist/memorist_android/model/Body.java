b8d4f5ee10e758a035dc61f69703e2c8d345bed3
berke-esmer-2015400021 <berke.esmer@boun.edu.tr>
2019-01-01 23:48:43 +0300

    Body model implemented.

---
+++ memorist-android/app/src/main/java/com/memorist/memorist_android/model/Body.java
@@ -0,0 +1,28 @@
+package com.memorist.memorist_android.model;
+
+import com.google.gson.annotations.Expose;
+
+public class Body {
+
+    @Expose
+    private String b_type;
+
+    @Expose
+    private String value;
+
+    public String getB_type() {
+        return b_type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setB_type(String b_type) {
+        this.b_type = b_type;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
